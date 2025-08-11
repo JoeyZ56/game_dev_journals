1bc467f3cf2b99634f47b2aa5cfb23d5bb7481dc
username <Jzazzi56@gmail.com>
2025-08-11 12:41:18 -0700

    learning the basics what main is, how to include the standard library, how to get the code to out put the console, and why we end the process with return 0

---
+++ intro_to_C++/hello_world/main.cpp
@@ -0,0 +1,14 @@
+//include the standard libray
+#include <iostream> 
+//input output stream (for printing on screen or getting something from user)
+
+//main is the entry point to the program
+int main() {
+    //standard library (std)
+    //character out (cout)
+    std::cout << "hello world!"; // this is a statement
+
+    return 0; //return 0 to terminate correctly
+
+
+}
