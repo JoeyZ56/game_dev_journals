19649f2fbdc7b7f21ee47788ec6e1e243fc69cb1
username <Jzazzi56@gmail.com>
2025-08-11 14:04:55 -0700

    mathematical opperators and how they work

---
+++ intro_to_C++/basics/mathematical_expressions/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main()
+{
+
+    int x = 10;
+    //double y = 3;
+    // int z = x + y; Add
+        //Output: z = 13
+    // int z = x - y; Subtract
+        //Output: z = 7
+    // int z = x * y; Multiply
+        //Output: z = 30
+    // int z = x / y; division
+    //  Division will return a full number unless one of the variables is a double (float)
+        //Output: z = 3.333
+    // int z = x % y; modular (returns the remainder)
+        //Output: z = 1
+
+    // std::cout << z;
+
+    //Postfix (changes z to equal the new amount but keeps the original (x) the same)
+    //int z = x++; increment (++)
+        //Output: z = 11
+    //int z = x--; decrement (--)
+        //Output: z = 9
+
+    //Prefix (changes both z and x to the new amount mutating both of the data)
+     //int z = ++x; increment (++)
+        //Output: z = 11, x = 11
+    //int z = --x; decrement (--)
+        //Output: z = 9, x = 9
+
+
+    //std::cout << z;
+
+    return 0;
+}
