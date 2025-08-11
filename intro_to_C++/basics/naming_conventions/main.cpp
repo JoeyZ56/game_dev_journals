38643c658d40408921db9b5fbc0e0e6ac1924c9d
username <Jzazzi56@gmail.com>
2025-08-11 13:34:46 -0700

    the different type of naming conventions used in creating variables and objects

---
+++ intro_to_C++/basics/naming_conventions/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+int file_size; // Snake Case
+int FileSize; // Pascal Case
+int fileSize; // Camel Case
+int iFileSize; // Hungarian Notation (old not relevent) putting the datatype before the variable name (example: i for int)
+    return 0;
+}
